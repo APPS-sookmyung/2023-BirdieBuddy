1902ce9270be09a348fc08ad1c3db28c1ba5e5db
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2023-09-22 14:11:14 +0900

    Version 0.2 RecordPage
    - 로그 추가와 삭제 기능을 추가함
    - 탐조 날짜와 위치 기록 칸은 따로 둠
    - 서브 제목 추가함
    
    + form을 만들기
    + 등록 버튼이 눌리면 페이지 속 내용이 json으로 바뀌어 기록 페이지로 옮겨지는 방식 연구하기
    + 사진이 업로드 되는 것을 구현/연구하기
    + 탐조 로그를 조회하는 기록 페이지 만들어야 함

--- RecordPage/record.js
+++ RecordPage/record.js
@@ -1,3 +1,5 @@
+const $ = (selector) => document.querySelector(selector);
+
 const pageButton = document.getElementsByClassName("pageButton");
 for (let i=0; i<pageButton.length;i++){
     if (pageButton[i].id != "currentButton"){
@@ -6,9 +8,9 @@ for (let i=0; i<pageButton.length;i++){
     }
 }
 
-const photo = document.getElementById("photo");
-const file = document.getElementById("file");
-const addImg = document.getElementById("addImg");
+const photo = $(".photo");
+const file = $(".file");
+const addImg = $(".addImg");
 photo.addEventListener("click",function(){
     file.click()
 })
@@ -17,10 +19,27 @@ file.addEventListener("change",function(){
     addImg.style.backgroundImage = `url(${this.value[0]})`;
 })
 
-const place = document.getElementById("place");
-place.addEventListener("input",resizeInput);
-resizeInput.call(input);
+//탐조 위치: 쓰는 글에 따라 배경 흰색 부분이 같이 커지는 방힉
 function resizeInput() {
     this.style.width = (this.value.length+8) + "ch";
 }
+const place = $(".place");
+place.addEventListener("input",resizeInput);
 
+
+//+ 버튼을 누르면 새로운 로그가 나타나는 코드
+const addedRecord = $(".birdFrame").outerHTML;
+$("#addButton")
+    .addEventListener("click",()=>{
+        $(".add")
+            .insertAdjacentHTML("beforeend",addedRecord)
+    })
+
+//- 버튼을 누르면 로그가 삭제되는 코드
+$(".add")
+    .addEventListener("click",(e)=>{
+        if (e.target.className == "deletebutton"){
+            console.log("Hey!");
+            e.target.closest(".birdFrame").remove();
+        }
+    })
