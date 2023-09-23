30c2d929a55221abd715b12661fdc20c351295e7
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2023-09-23 21:44:37 +0900

    Version 0.3 RecordPage
    - 등록버튼 새가 hover되면 그림이 바뀌도록 만듦
    - 사진이 추가 되도록 만듦
    - 페이지의 모든 내용이 json에 저장되게 만듦
    - 탐조 기록 열람 페이지 열고 페이지간 연결을 고침
    - 이미지 파일이 로드되면 총 몇개 받았는지 표시 되게 만듦
    - 왼쪽 오른쪽 방향키 누르면 숫자가 바뀌게 함
    
    + form을 만들기
    + 왼쪽 오른쪽 방향키에 따라서 열람하는 사진도 달라지게 만들기
    + 백엔드와 연결해보기

--- RecordPage/record.js
+++ RecordPage/record.js
@@ -1,3 +1,4 @@
+//약식 구축하기
 const $ = (selector) => document.querySelector(selector);
 
 const pageButton = document.getElementsByClassName("pageButton");
@@ -8,38 +9,128 @@ for (let i=0; i<pageButton.length;i++){
     }
 }
 
-const photo = $(".photo");
-const file = $(".file");
-const addImg = $(".addImg");
-photo.addEventListener("click",function(){
-    file.click()
-})
-file.addEventListener("change",function(){
-    photo.style.display = "none";
-    addImg.style.backgroundImage = `url(${this.value[0]})`;
-})
 
-//탐조 위치: 쓰는 글에 따라 배경 흰색 부분이 같이 커지는 방힉
+//탐조 위치: 쓰는 글에 따라 배경 흰색 부분이 같이 커지는 코드
 function resizeInput() {
     this.style.width = (this.value.length+8) + "ch";
 }
-const place = $(".place");
+const place = $("#place");
 place.addEventListener("input",resizeInput);
 
 
-//+ 버튼을 누르면 새로운 로그가 나타나는 코드
+//+ 버튼을 누르면 새로운 로그가 나타나는 코드 (로그 생성과 동시에 json object 추가)
+//json을 부르는 코드. assert, {type:'json'}이 없으면 에러가 뜬다. 아 참, html에도 type="module"을 넣어줘야한다.
+import data from './record.json' assert { type: 'json' };
+
 const addedRecord = $(".birdFrame").outerHTML;
 $("#addButton")
     .addEventListener("click",()=>{
-        $(".add")
-            .insertAdjacentHTML("beforeend",addedRecord)
-    })
+        $(".add").insertAdjacentHTML("beforeend",addedRecord)
+        data[1].Birds.push({"Birdname":"", "Explanation":"", "Photos":[], "Id":""});
+        console.log(data);
+})
 
-//- 버튼을 누르면 로그가 삭제되는 코드
+
+
+//input type file을 누를때 쓸 함수이다. (대표 사진이 바뀌고, file에서 받은 사진들을 json에 저장하는 코드)
+function changePhoto(target){
+    target.addEventListener("change",()=>{
+        const file = target.files[0];
+        //FileReader이란걸 쓴다. 
+        const reader = new FileReader();
+        const allFiles = document.querySelectorAll(".file");
+        const allInitPhotos = document.querySelectorAll(".photo");
+        const allNumbers = document.querySelectorAll(".number");
+        const allTotals = document.querySelectorAll(".total");
+
+        //내가 클릭한 file이 몇번째 file 버튼일까 구하는 코드
+        const index = Array.prototype.indexOf.call(allFiles,target);
+        console.log(index);
+
+        reader.addEventListener("load",()=>{
+            //처음에 allInitPhotos[index]말고 그냥 $(".photos")했더니만 첫번째 file 대표사진만 바뀌더라. 이런 실수.
+            allInitPhotos[index].src = reader.result;
+            allInitPhotos[index].style.width = "8vw";
+            allInitPhotos[index].style.height = "auto";
+        })
+        if (file){
+            reader.readAsDataURL(file);
+        }
+        allNumbers[index].innerText = 1;
+        allTotals[index].innerText = target.files.length;
+        //json에 저장하기   
+        data[1].Birds[index].Photos = target.files;
+    })
+}
+
+//부모에 위임해서 JS로 새로 생긴 HTML에게도 eventListen 할수 있게 해주는 코드
 $(".add")
     .addEventListener("click",(e)=>{
-        if (e.target.className == "deletebutton"){
-            console.log("Hey!");
+        //- 버튼을 누르면 로그가 삭제되는 코드
+        if (e.target.className == "deletebutton" && document.querySelectorAll(".birdFrame").length>1){
             e.target.closest(".birdFrame").remove();
         }
+        if (e.target.className == "file"){    
+            //파일 버튼 누르면 인풋 파일이 작동하는 코드
+            changePhoto(e.target);
+        }
+        if (e.target.className == "prevImg"){
+            const allPrevImg = document.querySelectorAll(".prevImg");
+            const index = Array.prototype.indexOf.call(allPrevImg,e.target);
+            const allNumbers = document.querySelectorAll(".number");
+            const total = document.querySelectorAll(".total")[index].innerText;
+            const allInitPhotos = document.querySelectorAll(".photo");
+            if (allNumbers[index].innerText==1){
+                allNumbers[index].innerText = total;
+            }
+            else{
+                allNumbers[index].innerText--;
+            }
+
+            //사진바꾸기 술
+            const reader = new FileReader();
+            reader.addEventListener("click",()=>{
+                allInitPhotos[index].src = reader.result;
+            })
+            console.log(data[1].Birds[index].Photos[allNumbers[index].innerText]);
+            reader.readAsDataURL(data[1].Birds[index].Photos[allNumbers[index].innerText]);
+        }
+        if (e.target.className=="nextImg"){
+            const allNextImg = document.querySelectorAll(".nextImg");
+            const index = Array.prototype.indexOf.call(allNextImg,e.target);
+            const allNumbers = document.querySelectorAll(".number");
+            const total = document.querySelectorAll(".total")[index].innerText;
+            const allInitPhotos = document.querySelectorAll(".photo");
+            if (allNumbers[index].innerText==total){
+                allNumbers[index].innerText = 1;
+            }
+            else{
+                allNumbers[index].innerText++;
+            }
+        }
     })
+
+//왼쪽 오른쪽 버튼 누르면 다른 사진으로 넘어가는 코드
+
+
+$(".total")
+
+//Submit 누르면 날짜와 장소를 json파일로 옮기기
+$("#addRecordBirdButton")
+    .addEventListener("click",()=>{
+        data[0].Date = $("#date").value;
+        data[0].Place = $("#place").value;
+        const birdNames = document.querySelectorAll(".birdName")
+        const writings = document.querySelectorAll(".write")
+        for (let i=0; i<birdNames.length; i++){
+            console.log(data[1].Birds[0]);
+            data[1].Birds[i].Birdname = birdNames[i].value;
+            data[1].Birds[i].Explanation = writings[i].value;
+            data[1].Birds[i].Id = i+1;
+            //"Birdname":"","Explanation":"","Photos":[],"Id":1
+        }
+        console.log(JSON.stringify(data))
+            
+        setTimeout(()=>{location.href = "../ViewRecordPage/viewRecord.html";},2000);
+        
+})
