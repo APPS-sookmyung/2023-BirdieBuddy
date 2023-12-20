dd38c1fdeea7a7d596ebb6747b7dae46f382473c
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2023-12-20 21:32:58 +0900

    [Feat] Add Animation to StartPage
    - Added animation in StartPage so that image slides in from the left

--- StartPage/start.js
+++ StartPage/start.js
@@ -1,5 +1,10 @@
-const pic = document.getElementById("startingpageImage");
-let num = 0
+import { $ } from "../src/util.js";
+
+const pic = $("#startingpageImage");
+const nextpic = $("#nextImage");
+
+let num = 0;
+let num2 = 1;
 const piclst = ["괭이갈매기.jpg","긴꼬리딱새.jpg","꼬까참새.jpg","흰눈썹황금새.jpg"];
 function changePic(){
     console.log("changed!");
@@ -7,7 +12,13 @@ function changePic(){
     num++;
     if (num==4){num = 0;}
 }
-setInterval(changePic,1*1000*10)
+function nextPic(){
+    nextpic.src = piclst[num2];
+    num2++;
+    if (num2==4){num2 = 0;}
+}
+setInterval(changePic,1*1000*10);
+setInterval(nextPic, 1*1000*10);
 
 const startPage = document.getElementById("startPage");
 function openPage(){
