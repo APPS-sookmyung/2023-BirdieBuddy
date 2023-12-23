c982038754858817cd53f6dbd75176a18ded4ba5
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2023-12-23 18:49:19 +0900

    [Feat] Connect Dummy Data with Encyclopedia Page
    DONE
    - connect dummy data with Encyclopedia page
    - make birdInfo appear according to searchBoxInput input (reactive page)

--- EncyclopediaPage/encyclopedia.js
+++ EncyclopediaPage/encyclopedia.js
@@ -1,3 +1,4 @@
+import { $ } from '../src/util.js';
 const pageButton = document.getElementsByClassName("pageButton");
 for (let i=0; i<pageButton.length;i++){
     if (pageButton[i].id != "currentButton"){
@@ -5,3 +6,45 @@ for (let i=0; i<pageButton.length;i++){
     pageButton[i].addEventListener("mouseout",function(){this.style.backgroundColor = "#DAE2B6";})
     }
 }
+
+let encyclopedia = [];
+fetch('./encyclopedia.json')
+    .then((response) => response.json())
+    .then((json) => {return json["data"].sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))})
+    .then((sorted)=>{sorted.forEach(sort=>encyclopedia.push(sort)); addEncyclopediaLog(sorted)})
+
+function addEncyclopediaLog(dataLog){
+    dataLog.forEach(data => {
+        let addData = `
+        <div class="birdInfo">
+            <image class="birdPic" src="${data["image"]}"></image>
+            <div class="birdText">
+                <a href="../BirdPage/bird.html"><p class="birdName">${data["name"]}<span class="birdSciName">(${data["sci_name"]})</span></p></a>
+                <p class="birdExplanation">${data["content"]}</p>
+            </div>
+        </div>
+        `;
+        $("#main").insertAdjacentHTML("beforeend", addData);
+    });
+}
+
+$("#searchBoxInput").addEventListener("keyup", ()=>{
+    console.log("pressed!");
+    document.querySelectorAll(".birdInfo").forEach(info=>{info.remove();})
+    let search = $("#searchBoxInput").value;
+    
+    encyclopedia.forEach(data => {
+        if (data["name"].includes(search)){
+            let addData = `
+            <div class="birdInfo">
+                <image class="birdPic" src="${data["image"]}"></image>
+                <div class="birdText">
+                    <a href="../BirdPage/bird.html"><p class="birdName">${data["name"]}<span class="birdSciName">(${data["sci_name"]})</span></p></a>
+                    <p class="birdExplanation">${data["content"]}</p>
+                </div>
+            </div>
+            `;
+            $("#main").insertAdjacentHTML("beforeend", addData);
+        }
+    })
+})
