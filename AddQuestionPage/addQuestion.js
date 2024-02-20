22761fe5996ed609df32e84841ccebabe649abad
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2024-02-20 19:01:52 +0900

    [Feat] Created addQuestion Page

--- AddQuestionPage/addQuestion.js
+++ AddQuestionPage/addQuestion.js
@@ -2,14 +2,33 @@ import {$} from "../src/util.js";
 import {buttonline} from "../src/buttonline.js"
 buttonline();
 
+function pauseEvent(e){
+    if(e.stopPropagation) e.stopPropagation();
+    if(e.preventDefault) e.preventDefault();
+    e.cancelBubble=true;
+    e.returnValue=false;
+    return false;
+}
+$("#cropWorkSpace").addEventListener("mousedown",(e)=>{pauseEvent(e)});
+$("#cropWorkSpace").addEventListener("mousemove",(e)=>{pauseEvent(e)});
+
+
 var openFile = function(file) {
     var input = file.target;
     var reader = new FileReader();
     reader.onload = function(){
-      var dataURL = reader.result;
-      var output = document.getElementById('output');
-      output.src = URL.createObjectURL(input.files[0]);
-      output.style.backgroundCOlor = "red";
+        var dataURL = reader.result;
+        for(let i=0; i<input.files.length; i++){
+            let addedImg = `
+            <div class="img" onmouseover="displayOptions(event)" onmouseleave="hideOptions(event)">
+                <div class="imgOptions">
+                    <img src="../UI/greyStar.png" class="option greyStar" onclick="changeStar(event)">
+                    <img src="../UI/X.png" class="option" onclick="deletePic(event)">
+                </div>
+                <img src="` + URL.createObjectURL(input.files[i]) + `" class="output">
+            </div>`
+            $("#imgs").insertAdjacentHTML("beforeend",addedImg);
+        }
     };
     reader.readAsDataURL(input.files[0]);
 };
@@ -17,3 +36,4 @@ var openFile = function(file) {
 $("#fileButton").addEventListener("change", (e)=>{
     openFile(e);
 });
+
